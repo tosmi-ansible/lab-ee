e72f9cda0dfa65af9767676d63db10098c1e92b8
Toni Schmidbauer <toni@stderr.at>
2025-01-06 12:45:45 +0100

    update

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "A basic flake with a shell";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs.systems.url = "github:nix-systems/default";
+  inputs.flake-utils = {
+    url = "github:numtide/flake-utils";
+    inputs.systems.follows = "systems";
+  };
+
+  outputs =
+    { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = [
+            pkgs.bashInteractive
+            pkgs.pre-commit
+            pkgs.gitleaks
+
+            pkgs.ansible
+            pkgs.ansible-lint
+            pkgs.ansible-navigator
+            pkgs.ansible-builder
+            pkgs.glibcLocales
+            pkgs.yamllint
+          ]; };
+      }
+    );
+}
